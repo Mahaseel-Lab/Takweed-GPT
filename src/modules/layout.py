7a6aa51140c92945a2579aba95f5d2c3dab5091c
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2023-07-17 09:42:06 +0300

    add streamlit

--- src/modules/layout.py
+++ src/modules/layout.py
@@ -13,4 +13,39 @@ class Layout:
             unsafe_allow_html=True,
         )
 
- 
+    def show_api_key_missing(self):
+        """
+        Displays a message if the user has not entered an API key
+        """
+        st.markdown(
+            """
+            <div style='text-align: center;'>
+                <h4>Enter your <a href="https://platform.openai.com/account/api-keys" target="_blank">OpenAI API key</a> to start chatting</h4>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+        hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+        st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+    def prompt_form(self):
+        """
+        Displays the prompt form
+        """
+        with st.form(key="my_form", clear_on_submit=True):
+            user_input = st.text_area(
+                "Query:",
+                placeholder="Ask me anything about the document...",
+                key="input",
+                label_visibility="collapsed",
+            )
+            submit_button = st.form_submit_button(label="Send")
+            
+            is_ready = submit_button and user_input
+        return is_ready, user_input
+    
